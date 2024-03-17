cdce37e14b0910154d0e373b63b366bd464e7092
eliaspacifico6 <eliaspacifico6@gmail.com>
2024-03-17 13:23:28 -0300

    Aula de Enumeração

---
+++ enum1-csharp/Course/Course/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Entities.Enums;
+
+namespace Course.Entities {
+    internal class Order {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return $"{Id}, {Moment}, {Status}";
+        }
+
+    }
+}
---
+++ enum1-csharp/Course/Course/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order() {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+            Console.WriteLine(order);
+
+            Console.WriteLine("=================");
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+
+            Console.WriteLine( os);
+            Console.WriteLine(txt);
+
+        }
+    }
+}
