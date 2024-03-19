ab9f45384c22cd5bd1878f7e499b691c4d33ba94
eliaspacifico6 <eliaspacifico6@gmail.com>
2024-03-19 13:21:44 -0300

    Exercicios 2 Composição

---
+++ enum2-csharp/Exercice/Exercice/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice.Entities {
+    internal class Comment {
+
+        public String text { get; set; }
+
+
+        public Comment(string text) {
+            this.text = text;
+        }
+    }
+}
